3991c286c57950516b2a07097e90e56a45bb9457
Khalil Khalil <khalil.t.khalil@gmail.com>
2023-09-28 14:09:32 -0500

    add navbar contents

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import { Outlet } from "react-router-dom";
 
 function App() {
   return (
-    <div data-theme="wireframe">
+    <div data-theme="cupcake">
       <Navbar />
       <Outlet />
     </div>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     extend: {},
   },
   daisyui: {
-    themes: ["wireframe", "dark"],
+    themes: ["wireframe", "dark", "cupcake"],
   },
   plugins: [require("daisyui")],
 };
