c344eb91aea931dd68abf12231bb281b78d5db83
Khalil Khalil <khalil.t.khalil@gmail.com>
2023-09-28 13:23:14 -0500

    add react-router

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./index.css";
+
 import { Counter } from "./features/counter/Counter";
 
 function App() {
   return (
     <div data-theme="wireframe">
-      <h1 className="text-3xl"> Hello Lets go</h1>
+      <h1 className="text-3xl"> sssHello Lets go</h1>
     </div>
   );
 }
