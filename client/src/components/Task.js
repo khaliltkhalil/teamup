5e970a1e19dbcc6f58d5fb41765768b2fe78ae5d
Khalil Khalil <khalil.t.khalil@gmail.com>
2023-10-07 12:50:02 -0500

    add delete button to task

--- client/src/components/Task.js
+++ client/src/components/Task.js
@@ -2,10 +2,10 @@ import React, { useEffect, useState } from "react";
 import UserCard from "./UserCard";
 import { useDispatch } from "react-redux";
 import { editTask } from "../features/tasksSlice";
+import { AiFillDelete } from "react-icons/ai";
 
 function Task({ id, title, user, status, currentUser }) {
   const [selectedStatus, setSelectedStatus] = useState(status);
-  console.log(status);
   const dispatch = useDispatch();
   const onOptionChange = (e) => {
     dispatch(
@@ -22,13 +22,14 @@ function Task({ id, title, user, status, currentUser }) {
         <h2>{title}</h2>
       </section>
       <UserCard {...user} />
-      <section>
+      <section className="w-1/4">
         <h2>status:</h2>
         <form>
           <div className="flex flex-col gap-2">
             <div className="flex flex-row gap-2">
               <label htmlFor={`pending-${id}`}>Pending</label>
               <input
+                disabled={currentUser.id === user.id ? false : true}
                 type="radio"
                 id={`pending-${id}`}
                 name="status"
@@ -41,6 +42,7 @@ function Task({ id, title, user, status, currentUser }) {
             <div className="flex flex-row gap-2">
               <label htmlFor={`ongoing-${id}`}> Ongoing</label>
               <input
+                disabled={currentUser.id === user.id ? false : true}
                 type="radio"
                 id={`ongoing-${id}`}
                 name="status"
@@ -53,6 +55,7 @@ function Task({ id, title, user, status, currentUser }) {
             <div className="flex flex-row gap-2">
               <label htmlFor={`completed-${id}`}>Completed</label>
               <input
+                disabled={currentUser.id === user.id ? false : true}
                 type="radio"
                 name="status"
                 id={`completed-${id}`}
@@ -64,6 +67,9 @@ function Task({ id, title, user, status, currentUser }) {
             </div>
           </div>
         </form>
+      </section>
+      <section className="flex w-1/4 justify-end ">
+        <AiFillDelete size={30} />
       </section>
     </div>
   );
